a62ea6d77fd81c9a659ab3faa5e5dfc1c2820772
mercersoft <philipp.h.schmid@gmail.com>
2025-09-15 14:40:25 -0700

    feat: create initial CycoTui project structure
    - Create cycotui/ directory with .NET solution
    - Add CycoTui.Core, CycoTui, CycoTui.Tests, and CycoTui.Examples projects
    - Configure solution with all projects
    - Add comprehensive .gitignore for C# projects
    
    This establishes the foundation for the CycoTui C# port of Ratatui.

---
+++ cycotui/src/CycoTui.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CycoTui.Core;
+
+public class Class1
+{
+
+}
---
+++ cycotui/tests/CycoTui.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CycoTui.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
